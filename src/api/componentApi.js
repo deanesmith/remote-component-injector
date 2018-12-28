cb400643d5edace4e66d316a507e43357d882da0
deanesmith <dsmith@cloudbees.com>
2018-12-28 15:49:52 -0500

    Stubbed out real API call and instead just return some static content for demonstration purposes.

--- src/api/componentApi.js
+++ src/api/componentApi.js
@@ -5,6 +5,10 @@ import axios from 'axios';
  * @returns {Promise<*>}
  */
 export function getComponent() {
-    const url = `http://localhost:3999/index.js`;
-    return axios.get(url).then(res=>res.data);
+    // Stub out api call to static api
+    //const url = `http://localhost:3999/index.js`;
+    //return axios.get(url).then(res=>res.data);
+    return new Promise(function (resolve) {
+        resolve("<div>This is the thing to be injected</div>"); // fulfilled
+    });
 }
